e43d69c80e2936e9613b910fcf6d8668250105f3
mike <mikeymaxdb@users.noreply.github.com>
2020-10-28 11:22:29 -0700

    added media keys to adjust layer

--- keyboards/crkbd/keymaps/mikeymaxdb/rules.mk
+++ keyboards/crkbd/keymaps/mikeymaxdb/rules.mk
@@ -1,6 +1,6 @@
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 RGBLIGHT_ENABLE = no
-RGB_MATRIX_ENABLE = WS2812
+#RGB_MATRIX_ENABLE = WS2812
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./keymaps/mikeymaxdb/glcdfont.c \
